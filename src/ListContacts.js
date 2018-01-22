77549f44c314b9b161eb2042fc54c849bcbfc054
anjali0626 <a9sharma@scu.edu>
2018-01-21 17:45:48 -0800

    Make search bar functionality work withhelp of escape-string-regexp and sort-by package

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import EscapeRegExp from 'escape-string-regexp'
+import SortBy from 'sort-by'
+
 
 // function ListContacts(props) {
 
@@ -44,34 +47,89 @@ class ListContacts extends Component {
     deleteContact: PropTypes.func.isRequired
   }
 
+  state = {
+    query: ''
+  }
+
+  updateQuery = (query) => {
+    this.setState({
+      query: query
+    })
+  }
+
+  clearQuery = () => {
+    this.setState({
+      query: ''
+    })
+  }
+
   render() {
+    const { contacts, deleteContact} = this.props
+    const { query } =  this.state
+
+
+    let showingContacts
+    if(query) {
+      const match = new RegExp(EscapeRegExp(query), 'i')
+      showingContacts = contacts.filter((contact) => match.test(contact.name))
+    } else {
+      showingContacts = contacts
+    }
+
+    showingContacts.sort(SortBy('name'))
+
+
     return (
-      <ol className='contact-list'>
-        {this.props.contacts.map((contact) => (
-          <li
-            key={contact.id}
-            className='contact-list-item'>
-              <div
-                className='contact-avatar'
-                style={{
-                  backgroundImage : `url(${contact.avatarURL})`
-                }}>
-              </div>
+      <div className='list-contacts'>
+        <div className='list-contacts-top'>
+          <input
+            className='search-contacts'
+            type='text'
+            placeholder='Search contacts'
+            value={query}
+            onChange={(event) => this.updateQuery(event.target.value)}
+          />
+        </div>
 
-              <div
-                className='contact-details'>
-              <p>{contact.name}</p>
-              <p>{contact.email}</p>
-              </div>
+        {showingContacts.length !== contacts.length && (
+          <div className='showing-contacts'>
+            <span>
+              Now Showing {showingContacts.length} of {contacts.length} total
+            </span>
+            <button onClick={this.clearQuery}>
+              Show all
+            </button>
+          </div>
+        )}
 
-              <button
-                onClick={() => this.props.deleteContact(contact)}
-                className='contact-remove'>
-                Remove
-              </button>
-          </li>
-          ))}
-      </ol>
+        <ol className='contact-list'>
+          {showingContacts.map((contact) => (
+            <li
+              key={contact.id}
+              className='contact-list-item'>
+                <div
+                  className='contact-avatar'
+                  style={{
+                    backgroundImage : `url(${contact.avatarURL})`
+                  }}>
+                </div>
+
+                <div
+                  className='contact-details'>
+                <p>{contact.name}</p>
+                <p>{contact.email}</p>
+                </div>
+
+                <button
+                  onClick={() => deleteContact(contact)}
+                  className='contact-remove'>
+                  Remove
+                </button>
+            </li>
+            ))}
+        </ol>
+      </div>
+
     )
 
   }
